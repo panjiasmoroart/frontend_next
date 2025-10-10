8f9c033758dec0eeef3e2267a8a56dbd5319db07
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-10-10 19:31:10 +0700

    Categories Section - Creating Categories Datatable

--- app/dashboard/categories/page.jsx
+++ app/dashboard/categories/page.jsx
@@ -1,7 +1,84 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Sheet } from "@/components/ui/sheet";
+import { DataTable } from "./features/data-table";
+import { columns } from "./features/columns";
+
+const data = [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "489e1d42",
+    amount: 125,
+    status: "processing",
+    email: "example@gmail.com",
+  },
+  // ...
+];
 
 const Page = () => {
-  return <div>Categories Page</div>;
+  const [sheetOpen, setSheetOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  return (
+    <div className="py-4 md:py-6 px-4 lg:px-6">
+      <Card className="@container/card">
+        <CardHeader>
+          <CardTitle>Categories</CardTitle>
+          <CardDescription>
+            <span>List of categories</span>
+          </CardDescription>
+
+          <CardAction>
+            <Button
+              onClick={() => {
+                setSelectedItem(null);
+                setSheetOpen(true);
+              }}
+            >
+              Add a new record
+            </Button>
+            <Sheet open={sheetOpen} onOpenChange={setSheetOpen}>
+              New
+              {/* <New
+                item={selectedItem}
+                isOpen={sheetOpen}
+                onSuccess={() => {
+                  setSheetOpen(false);
+                  fetchData();
+                }}
+              /> */}
+            </Sheet>
+          </CardAction>
+        </CardHeader>
+
+        <CardContent>
+          <DataTable columns={columns} data={data} />
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default Page;
